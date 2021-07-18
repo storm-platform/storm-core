42907c740ecb1c5598ec0f5e69dfbdac76e445c8
Felipe Carlos <felipe.carlos@fatec.sp.gov.br>
2021-07-18 20:26:49 +0000

    add initial example

---
+++ examples/lulc-classification/R/02_classify.R
@@ -0,0 +1,51 @@
+et.seed(777)
+library(sits)
+
+#
+# Auxiliary function
+#
+extract_ts_by_sample_location <- function(collection, start_date, end_date, bands, tiles, sample_file) {
+  cube <- sits_cube(
+    source      = "BDC",
+    name        = "cube_to_extract_sample",
+    url         = "https://brazildatacube.dpi.inpe.br/stac/",
+    collection  = collection,
+    start_date  = start_date,
+    end_date    = end_date,
+    bands       = bands,
+    tiles       = tiles
+  )
+  
+  samples <- sits_get_data(cube = cube, file = sample_file, multicores = 8)
+  samples
+}
+
+#
+# General definitions
+#
+start_date  <- "2018-09-14"
+end_date    <- "2019-07-28"
+sample_file <- "data/training-samples.csv"
+
+#
+# Output directory
+#
+output_dir <- paste0("bdc-article", "/training-samples")
+dir.create(
+  path         = output_dir,
+  showWarnings = FALSE,
+  recursive    = TRUE
+)
+
+#
+# CBERS-4/AWFI (16 days 'stack')
+#
+cb4_samples_with_ts <- extract_ts_by_sample_location(
+  collection  = "CB4_64_16D_STK-1",
+  start_date  = start_date,
+  end_date    = end_date,
+  bands       = c("BAND15", "BAND14", "BAND13", "BAND16", "NDVI", "EVI"),
+  sample_file = sample_file,
+  tiles       = c("022024")
+)
+saveRDS(cb4_samples_with_ts, paste0(output_dir, "/CB4_64_16D_STK_1.rds"))
