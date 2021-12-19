9bdcd9d59de615e05d207382f51e5a7cc078cadf
Phelps <phelpsmemo@gmail.com>
2021-12-19 16:43:24 -0300

    update test header

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,8 @@
+# -*- coding: utf-8 -*-
 #
-# This file is part of Brazil Data Cube Reproducible Research Management CLI.
-# Copyright (C) 2021 INPE.
+# Copyright (C) 2021 Storm Project.
 #
-# Brazil Data Cube Reproducible Research Management CLI is free software; you can redistribute it and/or modify it
+# storm-core is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
-#
 
-"""Brazil Data Cube Reproducible Research Management Tests."""
+"""Storm Core Tests."""
