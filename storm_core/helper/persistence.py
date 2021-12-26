5fea82ac86859aa188413c796e221efd9970f457
Phelps <phelpsmemo@gmail.com>
2021-12-26 13:20:39 -0300

    generic helper class to persist objects in pickle files (Closes #86)

--- storm_core/helper/persistence.py
+++ storm_core/helper/persistence.py
@@ -5,41 +5,86 @@
 # storm-core is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-"""SpatioTemporal Open Research Manager Core Persistence Operations."""
+import dill
+from pathlib import Path
 
 from typing import Union
 
-from igraph import Graph
 
+class PicklePersistenceContainer:
+    """A base (and generic) persistence container to store complete
+    objects into pickle files."""
 
-class GraphPersistencePickle(object):
-    """Graph persistence using pickle."""
-
-    @staticmethod
-    def save_graph(graph: Graph, pickle_file: str) -> None:
-        """Save a graph to a persistence store into a pickle file.
+    def __init__(self, obj: object):
+        """Initializer.
 
         Args:
-            graph (igraph.Graph): Graph to be saved.
-
-            pickle_file (str): Full path to the file where the graph will be saved.
-
-        Returns:
-            None: Graph is saved to a persistence store.
+            obj (object): Object to persist.
         """
-        graph.write_pickle(pickle_file)
+        self._obj = obj
 
-    @staticmethod
-    def load_graph(pickle_file: str) -> Union[Graph, None]:
-        """Load a graph from a persistence store.
+    @property
+    def obj(self):
+        return self._obj
+
+    @classmethod
+    def load(cls, path: Union[str, Path], **kwargs) -> object:
+        """Load a dumped Persistence Container object pickle.
 
         Args:
-            pickle_file (str): Full path to the pickle file that will be loaded.
+            path (Union[str, Path]): Path to the pickle file.
 
         Returns:
-            igraph.Graph: Graph loaded from a persistence store.
+            object: The loaded object.
         """
-        return Graph.Read_Pickle(pickle_file)
+        object_path = Path(path)
+
+        if not object_path.is_file():
+            raise FileNotFoundError(f"File not found: {object_path}")
+
+        with open(object_path, "rb") as ifile:
+            return dill.load(ifile, **kwargs).obj
+
+    @classmethod
+    def save(
+        cls,
+        obj: object,
+        path: Union[str, Path],
+        **kwargs,
+    ) -> Path:
+        """Persist an arbitrary object into a pickle file.
+
+        This function stores the given object in a ``PicklePersistenceContainer`` and
+        then save it in a pickle file.
+
+        Args:
+            obj (object): Object to persist.
+
+            path (Union[str, Path]): Path to the pickle file.
+
+            kwargs (dict): ``dill.dump`` extra arguments.
+
+        Returns:
+            Path: Path to the created pickle file.
+
+        Note:
+            This function uses the ``dill`` library to read/write pickle files.
+
+        See:
+            For more details about the ``dill.dump`` function, please check the
+            official documentation: https://dill.readthedocs.io/en/latest/
+
+        """
+        object_path = Path(path)
+        object_path.parent.mkdir(parents=True, exist_ok=True)
+
+        # saving the pickle object
+        persistence_container = cls(obj)
+
+        with open(object_path, "wb") as ofile:
+            dill.dump(persistence_container, ofile, **kwargs)
+
+        return object_path
 
 
-__all__ = "GraphPersistencePickle"
+__all__ = "PicklePersistenceContainer"
